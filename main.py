83151d3225cc434f71e9cfeeb655e468e42d00d8
nap123-sys <lukasm3@email.cz>
2024-10-04 11:47:25 +0200

    update daily. version: 00002

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+# updated daily :D
+
 sum_of_squares = 0
 N=10
 for i in range(1, N+1):
-    sum_of_squares += i***3
+    sum_of_squares += i**3
     
 print(sum_of_squares)
+
+result = 5648 + 74899292
+
+print(result)
